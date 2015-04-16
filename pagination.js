fd7e7197fe8054e165b05d4be3bbd0922e7e20a2
Paris Nakita Kejser <info@phptech.dk>
2015-04-16 10:55:40 +0200

    Add view.max function
    if you use this, you can use it to only view the last 5 pages, your current page and the next 5 eg. you can change this number if you want.

--- pagination.js
+++ pagination.js
@@ -14,7 +14,8 @@
 			},
 			view : {
 				next : true,
-				previous : true
+				previous : true,
+				max : 0
 			}
 		}, options );
 		
@@ -33,20 +34,72 @@
 				)
 				.appendTo( $(this) );
 			}
-		
-			for( i = 1; i <= settings.total; i++ )
+			
+			if ( settings.view.max == 0 )
 			{
+				for( i = 1; i <= settings.total; i++ )
+				{
+					$( document.createElement('li') )
+					.addClass( ( settings.current == i ? 'active' : '' ))
+					.append(
+						$( document.createElement('a') )
+						.attr('style', 'cursor: pointer;')
+						.attr('onclick', settings.url.link +'('+ i +')')
+						.html( i )
+					)
+					.appendTo( $( this ) );
+				}
+			}
+			else
+			{
+				var minList = parseInt(settings.current) - parseInt(settings.view.max);
+				var maxList = parseInt(settings.view.max) + parseInt(settings.current);
+				
+				if ( minList > -(settings.view.max) )
+				{
+					for(i=minList; i < settings.current; i++)
+					{
+						if (i > 0 )
+						{
+							$( document.createElement('li') )
+							.append(
+								$( document.createElement('a') )
+								.attr('style', 'cursor: pointer;')
+								.attr('onclick', settings.url.link +'('+ i +')')
+								.html( i )
+							)
+							.appendTo( $(this) );
+						}
+					}
+				}
+				
 				$( document.createElement('li') )
-				.addClass( ( settings.current == i ? 'active' : '' ))
+				.addClass('active')
 				.append(
 					$( document.createElement('a') )
-					.attr('style', 'cursor: pointer;')
-					.attr('onclick', settings.url.link +'('+ i +')')
-					.html( i )
+					.html(settings.current)
 				)
-				.appendTo( $( this ) );
+				.appendTo( $(this) );
+				
+				if ( maxList < ( parseInt(settings.total) + parseInt(settings.view.max) ) )
+				{
+					for(i=1; i <= settings.view.max; i++)
+					{
+						if ( (parseInt(settings.current)+i) <= settings.total )
+						{
+							$( document.createElement('li') )
+							.append(
+								$( document.createElement('a') )
+								.attr('style', 'cursor: pointer;')
+								.attr('onclick', settings.url.link +'('+ ( parseInt(settings.current) + i ) +')')
+								.html( ( parseInt(settings.current) + i ) )
+							)
+							.appendTo( $(this) );
+						}
+					}
+				}
 			}
-		
+			
 			if ( settings.view.previous == true && settings.current != null && settings.current < settings.total  )
 			{
 				$( document.createElement('li') )
