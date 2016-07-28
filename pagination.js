24eee3cd82a08acf2089c103319fac68337ee4b3
Paris Nakita Kejser <parisnakitakejser@icloud.com>
2016-07-28 09:42:28 +0200

    Upgrade to v. 1.1
    This version add support to jump fast to the last and the first page view.

--- pagination.js
+++ pagination.js
@@ -1,11 +1,10 @@
 /*!
- * Bootstrap Pagination v1 (http://parisnakitakejser.com)
- * Copyright 2015 - Paris Nakita Kejser.
+ * Bootstrap Pagination v1.1 (http://parisnakitakejser.com)
+ * Copyright 2015 - 2016 by Paris Nakita Kejser.
  */
 (function( $ ){
-	
-	$.fn.pagination = function(options)
-	{
+
+	$.fn.pagination = function(options) {
 		var settings = $.extend({
 			total : null,
 			current : null,
@@ -18,13 +17,11 @@
 				max : 0
 			}
 		}, options );
-		
+
 		$(this).empty();
-		
-		if ( settings.total != null && settings.current != null )
-		{
-			if ( settings.view.next == true && settings.current != null && settings.current > 1 )
-			{
+
+		if ( settings.total != null && settings.current != null ) {
+			if ( settings.view.next == true && settings.current != null && settings.current > 1 ) {
 				$( document.createElement('li') )
 				.append(
 					$( document.createElement('a') )
@@ -34,11 +31,29 @@
 				)
 				.appendTo( $(this) );
 			}
-			
-			if ( settings.view.max == 0 )
-			{
-				for( i = 1; i <= settings.total; i++ )
-				{
+
+
+			if(settings.current > (parseInt(settings.view.max)+1)) {
+				$( document.createElement('li') )
+				.append(
+					$( document.createElement('a') )
+					.attr('style', 'cursor: pointer;')
+					.attr('onclick', settings.url.link +'(1)')
+					.html(1)
+				)
+				.appendTo( $(this) );
+
+				$( document.createElement('li') )
+				.attr({
+					'style' : 'padding: 6px 12px; line-height: 1.428571429; text-decoration: none; color: #5D9FD7; background-color: #fff; border: 1px solid #ddd; margin-left: -1px; float: left; color: gray;'
+				})
+				.html('...')
+				.appendTo( $(this) );
+			}
+
+
+			if ( settings.view.max == 0 ) {
+				for( i = 1; i <= settings.total; i++ ) {
 					$( document.createElement('li') )
 					.addClass( ( settings.current == i ? 'active' : '' ))
 					.append(
@@ -49,19 +64,15 @@
 					)
 					.appendTo( $( this ) );
 				}
-			}
-			else
-			{
+			} else {
 				var minList = parseInt(settings.current) - parseInt(settings.view.max);
 				var maxList = parseInt(settings.view.max) + parseInt(settings.current);
-				
-				if ( minList > -(settings.view.max) )
-				{
+
+				if ( minList > -(settings.view.max) ) {
 					var i = 0;
-					for(i=minList; i < settings.current; i++)
-					{
-						if (i > 0 )
-						{
+
+					for(i=minList; i < settings.current; i++) {
+						if (i > 0 ) {
 							$( document.createElement('li') )
 							.append(
 								$( document.createElement('a') )
@@ -73,7 +84,7 @@
 						}
 					}
 				}
-				
+
 				$( document.createElement('li') )
 				.addClass('active')
 				.append(
@@ -81,13 +92,10 @@
 					.html(settings.current)
 				)
 				.appendTo( $(this) );
-				
-				if ( maxList < ( parseInt(settings.total) + parseInt(settings.view.max) ) )
-				{
-					for(i=1; i <= settings.view.max; i++)
-					{
-						if ( (parseInt(settings.current)+i) <= settings.total )
-						{
+
+				if ( maxList < ( parseInt(settings.total) + parseInt(settings.view.max) ) ) {
+					for(i=1; i <= settings.view.max; i++) {
+						if ( (parseInt(settings.current)+i) <= settings.total ) {
 							$( document.createElement('li') )
 							.append(
 								$( document.createElement('a') )
@@ -100,9 +108,26 @@
 					}
 				}
 			}
-			
-			if ( settings.view.previous == true && settings.current != null && settings.current < settings.total  )
-			{
+
+			if((parseInt(settings.total) - settings.view.max ) > settings.current) {
+				$( document.createElement('li') )
+				.attr({
+					'style' : 'padding: 6px 12px; line-height: 1.428571429; text-decoration: none; color: #5D9FD7; background-color: #fff; border: 1px solid #ddd; margin-left: -1px; float: left; color: gray;'
+				})
+				.html('...')
+				.appendTo( $(this) );
+
+				$( document.createElement('li') )
+				.append(
+					$( document.createElement('a') )
+					.attr('style', 'cursor: pointer;')
+					.attr('onclick', settings.url.link +'('+ parseInt(settings.total) +')')
+					.html(settings.total)
+				)
+				.appendTo( $(this) );
+			}
+
+			if ( settings.view.previous == true && settings.current != null && settings.current < settings.total  ) {
 				$( document.createElement('li') )
 				.append(
 					$( document.createElement('a') )
@@ -112,15 +137,13 @@
 				)
 				.appendTo( $(this) );
 			}
-		}
-		else
-		{
+		} else {
 			$( document.createElement('li') )
 			.html('You need to defined total or current settings')
 			.appendTo( $(this) );
 		}
-		
+
 		return this;
 	};
-	
+
 }(jQuery));
